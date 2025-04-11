954e18245ddcf846808dd3ed4b7add3ccd68e1cd
YePeOn7 <prakoso.yohan@gmail.com>
2025-04-11 07:12:40 +0000

    add install dependency

---
+++ install_dependency.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+udo apt-get update
+sudo apt-get install lsb-release gnupg
+
+sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+sudo apt-get install ignition-fortress
+
+sudo apt install -y ros-humble-ros-gz \
+  ros-humble-gz-ros2-control \
+  ros-humble-ros2-control \
+  ros-humble-ros2-controllers \
+  ros-humble-navigation2 \
+  ros-humble-nav2-bringup \
+  ros-humble-controller-manager
